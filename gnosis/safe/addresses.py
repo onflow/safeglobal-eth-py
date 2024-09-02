2b5ebd53bb08e82db0e5ee5dc131e13ba3bfd282
Den Smalonski <dzianis.smalonski@protofire.io>
2024-09-02 17:12:12 +0200

    feat: add flow testnet

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -340,6 +340,10 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 170908, "1.3.0+L2"),
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 170910, "1.3.0"),
     ],
+    EthereumNetwork.FLOW_TESTNET: [
+        ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 2346892, "1.3.0+L2"),
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 2346918, "1.3.0"),
+    ],
     EthereumNetwork.AURORA_MAINNET: [
         (
             "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",
@@ -2261,6 +2265,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.X_LAYER_MAINNET: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 359536),  # v1.3.0
+    ],
+    EthereumNetwork.FLOW_TESTNET: [
+        ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 2344624),  # v1.3.0
     ],
     EthereumNetwork.PULSECHAIN: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 12504126),  # v1.3.0
